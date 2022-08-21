38eb1149dc3e54a6d20ba15b5e1aae10c6b52b73
Maddy <22009843+AurySystem@users.noreply.github.com>
2022-08-21 11:04:46 -0400

    push old changes

--- src/main/java/aurysystem/aurumsabyss/Worldgen/Features/TreePlacer/BranchyTrunkPlacer.java
+++ src/main/java/aurysystem/aurumsabyss/Worldgen/Features/TreePlacer/BranchyTrunkPlacer.java
@@ -1,5 +1,6 @@
 package aurysystem.aurumsabyss.Worldgen.Features.TreePlacer;
 
+import aurysystem.aurumsabyss.Abyssal;
 import com.google.common.collect.Lists;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
@@ -39,23 +40,23 @@ public class BranchyTrunkPlacer extends TrunkPlacer {
         int x = startPos.getX();
         int y = startPos.getY();
         int z = startPos.getZ();
-        int x1 = x;
-        int z1 = z;
         int y1 = y + height;
-        float rad = 0.6f;
+        float rad = 1.5f;
         BranchyTrunkPlacer.setToDirt(world, replacer, random, blockPos, config);
         for(int x2 = -MathHelper.floor(rad); x2 <= MathHelper.ceil(rad); x2++){
             for(int z2 = -MathHelper.floor(rad); z2 <= MathHelper.ceil(rad); z2++){
                 BranchyTrunkPlacer.setToDirt(world, replacer, random, blockPos.add(x2,0,z2), config);
             }
         }
-        list.addAll(generateBranch(0,height,1,new BlockPos(y, y1+1, z),world,replacer,random,startPos,config,list,rad));
-        list.addAll(generateBranch(6,height,1.2f,new BlockPos(x+6, y1+1, z+6),world,replacer,random,startPos,config,list,rad));
+        list.addAll(generateBranch(0,height,0.075f,new BlockPos(x, y1+1, z),world,replacer,random,startPos,config,list,rad));
+        list.addAll(generateBranch(height/2,height/5*4,0.1f,new BlockPos(x+random.nextInt(12)-6, y+(height/5*4), z+random.nextInt(12)-6),world,replacer,random,startPos,config,list,0.2f));
 
 
         return list;
     }
     private List<FoliagePlacer.TreeNode> generateBranch(int startHeight,int height, float slope, BlockPos target, TestableWorld world, BiConsumer<BlockPos, BlockState> replacer, Random random, BlockPos startPos, TreeFeatureConfig config, List<FoliagePlacer.TreeNode> list, float rad) {
+
+        float hMax = height-startHeight;
         for (int heightI = startHeight; heightI < height; ++heightI) {
             int x = startPos.getX();
             int y = startPos.getY();
@@ -67,8 +68,14 @@ public class BranchyTrunkPlacer extends TrunkPlacer {
             int z2 = target.getZ();
 
             BlockPos blockPos2;
-            x1 = MathHelper.ceil(x2+(x2-x1)/slope);
-            z1 = MathHelper.ceil(z2+(z2-z1)/slope);
+
+            float hDiff = heightI-startHeight;
+            float scaler = (hDiff / hMax);
+
+            x1 = MathHelper.ceil(MathHelper.lerp(scaler,x,x2) );
+            z1 = MathHelper.ceil(MathHelper.lerp(scaler,z,z2) );
+            rad = rad-slope>0?rad-slope:0;
+            Abyssal.LOG(rad+" current branch radius");
             if (!TreeFeature.isAirOrLeaves(world, blockPos2 = new BlockPos(x1, y + heightI, z1))) continue;
             BranchyTrunkPlacer.getAndSetState(world, replacer, random, blockPos2, config);
             for (int x3 = -MathHelper.floor(rad); x3 <= MathHelper.ceil(rad); x3++) {
